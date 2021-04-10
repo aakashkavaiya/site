5adb78f1aac9c5bf194cba4e7284939a78f66fc8
Aakash Kavaiya <66742335+aakashkavaiya@users.noreply.github.com>
2021-04-10 20:00:54 +0530

    Update gen-js.js

--- gen-js.js
+++ gen-js.js
@@ -2,10 +2,11 @@ function AddName() {
   const names = document.getElementById("name").value;
   const gids = document.getElementById("gid").value;
   const tag = document.getElementById("tagname").value;
-  
+  VID_REGEX = /(?:drive.google(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:d)?)\/|\S*?[?&]id=))([a-zA-Z0-9_-]{33})/
+  const ngids = gids.match(VID_REGEX)[1]; 
   const rt1 = `if (x1 == 'https://aakashkavaiya.blogspot.com/p/pdf-download.html#${tag}' || x1 == 'https://aakashkavaiya.blogspot.com/p/pdf-download.html?m=1#${tag}')
 {
-  var x2 = '${gids}';
+  var x2 = '${ngids}';
   document.getElementById("123").innerHTML = '<center><iframe src="https://drive.google.com/file/d/' + x2 + '/preview" width="100%" height="550"></iframe><br></center>';
   document.getElementById('12').innerHTML = "${names}";
   document.getElementById('bt-pdf').innerHTML = '<center><a class="ak-bt bt-gr a-ak" href="https://drive.google.com/uc?export=download&id=' + x2 + '" target="_blank"><i class="fa fa-download" aria-hidden="true"></i> Download PDF</a><br></center>'
